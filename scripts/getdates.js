f7cd5d8261780effccf986e761de9c29fa25a60c
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-06-15 17:15:31 -0300

    ready

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,26 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Atualiza o ano atual
+document.addEventListener('DOMContentLoaded', function () {
     let yearSpan = document.getElementById('current-year');
     if (yearSpan) {
         yearSpan.textContent = new Date().getFullYear();
     }
 
-    // Atualiza a data da última modificação
     let lastModifiedSpan = document.getElementById('last-modified');
     if (lastModifiedSpan) {
-        // Função para formatar a data no formato desejado (opcional)
         function formatLastModified(date) {
-            // Formato padrão (altere conforme necessário)
-            return date.toLocaleString(); // Retorna data e hora formatada
+            return date.toLocaleString();
         }
 
-        // Atualiza a data da última modificação da página
         function updateLastModified() {
-            let now = new Date(); // Obtém a data e hora atuais
+            let now = new Date();
             lastModifiedSpan.textContent = formatLastModified(now);
         }
-
-        // Chamada inicial para definir a data da última modificação
         updateLastModified();
     }
 });
